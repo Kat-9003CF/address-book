44105ce4fdbe1c6dc727d6c66eb84f2be4a023f4
Kat Hurdley <katherine@luckyduck.co>
2023-03-09 01:23:34 +0000

    adds font files

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -9,11 +9,18 @@ const theme = {
 
   fonts: {
     robotoSlabLight: 'RobotoSlabLight, sans-serif',
+    robotoSlabRegular: 'RobotoSlabRegular, sans-serif',
+    robotoSlabBold: 'RobotoSlabBold, sans-serif',
   },
   fontSizes: {
     large: '61px',
     medium: '27px',
     small: '18px',
+  },
+  lineHeights: {
+    large: '81px',
+    medium: '35px',
+    small: '24px',
   },
 };
 
