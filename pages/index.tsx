1d4efb80c5f87069b46286c354f6fd044e415435
Kat Hurdley <katherine@luckyduck.co>
2023-03-09 01:24:39 +0000

    Adds columns and modal

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,43 @@
-import { useStore } from '../src/zustand/store';
 import { Container } from '../src/styles/generic';
 import styled from 'styled-components';
 import { H1 } from '../src/styles/typography';
+import { AddContactModal } from '../components/molecules';
 
 export default function Home() {
-  const { theme, setTheme } = useStore((state) => state);
-
   return (
-    <OuterWrapper>
-      <Container>
+    <Container>
+      <ContentWrapper>
         <H1>Address Book</H1>
-      </Container>
-    </OuterWrapper>
+        <FlexWrapper>
+          <ColumnWrapper>
+            <AddContactModal />
+          </ColumnWrapper>
+          <ColumnWrapper>
+            <H1>Input goes here</H1>
+          </ColumnWrapper>
+        </FlexWrapper>
+      </ContentWrapper>
+    </Container>
   );
 }
 
-const OuterWrapper = styled.div``;
+const ContentWrapper = styled.div`
+  border: 1px solid red;
+  width: 100%;
+`;
+
+const FlexWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  gap: 80px; //check size
+`;
+
+const ColumnWrapper = styled.div`
+  border: 1px solid purple;
+  .left {
+    flex: 25%;
+  }
+  .right {
+    flex: 75%;
+  }
+`;
