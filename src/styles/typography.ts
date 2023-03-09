ade9a94de31444d12641e4e14319bcc844bafbb0
Kat Hurdley <katherine@luckyduck.co>
2023-03-09 01:23:48 +0000

    adds styled fonts

--- src/styles/typography.ts
+++ src/styles/typography.ts
@@ -2,4 +2,30 @@ import styled from 'styled-components';
 
 export const H1 = styled.h1`
   font-family: ${(props) => props.theme.fonts.robotoSlabLight};
+  font-size: ${(props) => props.theme.fontSizes.large};
+  font-weight: 300;
+`;
+
+export const ModalHeader = styled.h1`
+  font-family: ${(props) => props.theme.fonts.robotoSlabRegular};
+  font-size: ${(props) => props.theme.fontSizes.medium};
+  line-height: ${(props) => props.theme.lineHeights.medium};
+  font-weight: 400;
+  margin: 0;
+`;
+
+export const BodyText = styled.p`
+  font-family: ${(props) => props.theme.fonts.robotoSlabBold};
+  font-size: ${(props) => props.theme.fontSizes.small};
+  line-height: ${(props) => props.theme.lineHeights.small};
+  font-weight: 700;
+  margin: 0;
+`;
+
+export const Placeholder = styled.p`
+  font-family: ${(props) => props.theme.fonts.robotoSlabRegular};
+  font-size: ${(props) => props.theme.fontSizes.small};
+  line-height: ${(props) => props.theme.lineHeights.small};
+  font-weight: 700;
+  margin: 0;
 `;
