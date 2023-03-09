c8501d1b4d27e16f1e7da411c73198e55effffc0
Kat Hurdley <katherine@luckyduck.co>
2023-03-09 22:41:27 +0000

    creates button

--- src/styles/generic.ts
+++ src/styles/generic.ts
@@ -8,3 +8,26 @@ export const Container = styled.div`
   align-items: center;
   padding: 60px 180px 0 120px;
 `;
+
+export const Button = styled.button`
+  background: ${(props) => props.theme.colours.teal};
+  border: 1px solid ${(props) => props.theme.colours.darkTeal};
+  border-radius: 4px;
+  width: 210px;
+  height: 44px;
+  font-family: ${(props) => props.theme.fonts.robotoSlabBold};
+  font-size: ${(props) => props.theme.fontSizes.small};
+  line-height: ${(props) => props.theme.lineHeights.small};
+  color: ${(props) => props.theme.colours.offWhite};
+  font-weight: 700;
+  margin: 0;
+  transition: 0.2s ease-in-out;
+  &:hover {
+    cursor: pointer;
+  }
+  &:disabled {
+    cursor: inherit;
+    border: 1px solid ${(props) => props.theme.colours.teal};
+    background: ${(props) => props.theme.colours.darkGrey};
+  }
+`;
