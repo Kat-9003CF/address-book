4da9fbc22a326184f1a4c56b1a39b8679ef79a3b
Kat Hurdley <katherine@luckyduck.co>
2023-03-10 01:52:14 +0000

    removes box shadow

--- components/atoms/TextInputField.tsx
+++ components/atoms/TextInputField.tsx
@@ -45,23 +45,22 @@ const InputWrapper = styled.div`
 `;
 
 const InputField = styled.input<{ error: boolean | undefined }>`
-    background: ${(props) => (props.error ? props.theme.colours.lightCoral : props.theme.colours.lightTeal)};
-    border: none;
-    padding: 12px;
-    width: 100%;
-    outline: none;
-    font-family: ${(props) => props.theme.fonts.robotoSlabRegular};
-    font-size: ${(props) => props.theme.fontSizes.small};
-    line-height: ${(props) => props.theme.lineHeights.small};
-    font-weight: 300;
-    color: ${({ theme }) => theme.colours.darkGrey};
+  background: ${(props) => (props.error ? props.theme.colours.lightCoral : props.theme.colours.lightTeal)};
+  border: none;
+  padding: 12px;
+  width: 100%;
+  outline: none;
+  font-family: ${(props) => props.theme.fonts.robotoSlabRegular};
+  font-size: ${(props) => props.theme.fontSizes.small};
+  line-height: ${(props) => props.theme.lineHeights.small};
+  font-weight: 300;
+  color: ${({ theme }) => theme.colours.darkGrey};
+  transition: 0.2s ease-in-out;
+  cursor: 'text';
+  &:focus {
+    border-radius: 4px;
+    background: none;
+    outline: 1px solid ${(props) => props.theme.colours.teal};
     transition: 0.2s ease-in-out;
-    cursor: 'text';
-    &:focus {
-      border-radius: 4px;
-      background: none;
-      outline: 1px solid ${(props) => props.theme.colours.teal};
-      box-shadow(0px 0px 3px ${({ theme }) => theme.colours.primary});
-      transition: 0.2s ease-in-out;
-    }
-  `;
+  }
+`;
