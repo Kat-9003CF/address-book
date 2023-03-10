788878aec333abc0d62b2e1cd2504c8feed75cfb
Kat Hurdley <katherine@luckyduck.co>
2023-03-10 00:07:14 +0000

    renames store and adds contact details

--- src/zustand/store.ts
+++ src/zustand/store.ts
@@ -1,11 +1,24 @@
+/* eslint-disable no-unused-vars */
 import create from 'zustand';
 
 interface StoreState {
   theme: boolean;
   setTheme: () => void;
+  firstName: string;
+  setFirstName: (firstName: string) => void;
+  lastName: string;
+  setLastName: (lastName: string) => void;
+  emailAddress: string;
+  setEmailAddress: (emailAddress: string) => void;
 }
 
-export const useStore = create<StoreState>((set) => ({
+export const useAddressStore = create<StoreState>()((set) => ({
   theme: true,
   setTheme: () => set((state) => ({ theme: !state.theme })),
+  firstName: '',
+  setFirstName: (firstName) => set(() => ({ firstName: firstName })),
+  lastName: '',
+  setLastName: (lastName) => set(() => ({ lastName: lastName })),
+  emailAddress: '',
+  setEmailAddress: (emailAddress) => set(() => ({ emailAddress: emailAddress })),
 }));
