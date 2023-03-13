ba8b5f8494c6b4c56744b22177be5bfc507122df
Kat Hurdley <katherine@luckyduck.co>
2023-03-13 11:37:53 +0000

    adds 'main' check to ensure page has loaded before testing

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -3,6 +3,7 @@ import 'cypress-axe';
 describe('Home Page', () => {
   it('Should be accessible', () => {
     cy.visit('/');
+    cy.get('[role="main"]').should('be.visible');
     cy.injectAxe();
     cy.checkA11y();
   });
