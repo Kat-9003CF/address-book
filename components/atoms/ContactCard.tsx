4a20a7641224059264765af52b00757d127c8333
Kat Hurdley <katherine@luckyduck.co>
2023-03-13 10:24:54 +0000

    adds mobile layout

--- components/atoms/ContactCard.tsx
+++ components/atoms/ContactCard.tsx
@@ -1,5 +1,7 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { BodyText, Placeholder } from '../../src/styles/typography';
+import { BREAKPOINT_SM, mq } from '../../src/utils/mq';
+import useWindowSize from '../../hooks/useWindowSize';
 
 export default function ContactCard({
   firstName,
@@ -10,12 +12,31 @@ export default function ContactCard({
   lastName: string;
   emailAddress: string;
 }) {
+  const windowSize = useWindowSize();
+  const isMobile: boolean = windowSize.width < BREAKPOINT_SM;
   return (
     <CardWrapper>
-      <CardText>Name</CardText>
-      <CardDetails>{`${firstName} ${lastName}`}</CardDetails>
-      <CardText>Email</CardText>
-      <CardDetails>{`${emailAddress}`}</CardDetails>
+      <CardContentWrapper>
+        {isMobile ? (
+          <GridWrapper>
+            <div>
+              <CardText>Name</CardText>
+              <CardDetails className="right-margin">{`${firstName} ${lastName}`}</CardDetails>
+            </div>
+            <div>
+              <CardText className="top-margin">Email</CardText>
+              <CardDetails>{`${emailAddress}`}</CardDetails>
+            </div>
+          </GridWrapper>
+        ) : (
+          <div>
+            <CardText>Name</CardText>
+            <CardDetails className="right-margin">{`${firstName} ${lastName}`}</CardDetails>{' '}
+            <CardText className="top-margin">Email</CardText>
+            <CardDetails>{`${emailAddress}`}</CardDetails>
+          </div>
+        )}
+      </CardContentWrapper>
     </CardWrapper>
   );
 }
@@ -24,21 +45,58 @@ const CardWrapper = styled.div`
   transition: 0.2s ease-in-out;
   border: 1px solid ${(props) => props.theme.colours.teal};
   border-radius: 4px;
-  padding: 35px 38px;
-  max-width: 284px;
-  max-height: 204px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  ${mq.tablet(css`
+    max-width: unset;
+    max-height: unset;
+    width: 100%;
+  `)};
+  ${mq.mobile(css`
+    padding: 0;
+    margin: 0 auto;
+  `)};
+`;
+
+const CardContentWrapper = styled.div`
+  width: 285px;
+  height: 204px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  ${mq.tablet(css`
+    height: fit-content;
+    margin: 16px;
+  `)};
+`;
+
+const GridWrapper = styled.div`
+  ${mq.mobile(css`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+  `)};
 `;
 
 const CardText = styled(BodyText)`
   color: ${(props) => props.theme.colours.teal};
-
-  &:nth-child(3) {
-    margin-top: 16px;
+  &.top-margin {
+    margin-top: 32px;
   }
+  ${mq.mobile(css`
+    &.top-margin {
+      margin: 0;
+    }
+  `)};
 `;
 
 const CardDetails = styled(Placeholder)`
   z-index: 1;
   position: relative;
   padding-left: 0;
+  margin: 0;
+  padding: 0;
+  ${mq.mobile(css`
+    margin-left: 0;
+  `)};
 `;
